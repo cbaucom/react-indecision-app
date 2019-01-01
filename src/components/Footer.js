33a833844630be8c5d0acf2f36ca21906560c635
Chris Baucom <crbaucom@gmail.com>
2019-01-01 18:22:25 -0500

    Migrate Indecision into create-react-app

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../logo.svg";
+
+const Footer = () => (
+  <footer className="App-footer">
+    <p>Indecision was built with React.</p>
+    <img src={logo} className="App-logo" alt="logo" />
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </footer>
+);
+
+export default Footer;
