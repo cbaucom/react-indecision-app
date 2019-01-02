6f6b090a5e43a1925404a70159ac8f61495aa9cb
Chris Baucom <crbaucom@gmail.com>
2019-01-01 23:21:26 -0500

    Fix disabled button hover

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import Option from './Option';
+import React from "react";
+import Option from "./Option";
 
-const Options = (props) => (
+const Options = props => (
   <div>
     <div className="widget-header">
       <h3 className="widget-header__title">Your Options</h3>
       <button
         className="button button--link"
         onClick={props.handleDeleteOptions}
+        disabled={props.options.length === 0}
       >
         Remove All
-    </button>
+      </button>
     </div>
 
-    {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
-    {
-      props.options.map((option, index) => (
-        <Option
-          key={option}
-          optionText={option}
-          count={index + 1}
-          handleDeleteOption={props.handleDeleteOption}
-        />
-      ))
-    }
+    {props.options.length === 0 && (
+      <p className="widget__message">Please add an option to get started!</p>
+    )}
+    {props.options.map((option, index) => (
+      <Option
+        key={option}
+        optionText={option}
+        count={index + 1}
+        handleDeleteOption={props.handleDeleteOption}
+      />
+    ))}
   </div>
 );
 
