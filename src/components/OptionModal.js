0b1429d6f372228370c93a788bffee4b353ded5d
Chris Baucom <crbaucom@gmail.com>
2019-01-02 02:25:31 -0500

    Using sweetalert2 for modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,24 +1,15 @@
 import React from "react";
-import Modal from "react-modal";
+// import Modal from "react-modal";
+import SweetAlert from "sweetalert2-react";
+import "../styles/components/_modal.scss";
 
 const OptionModal = props => (
-  <Modal
-    isOpen={!!props.selectedOption}
-    onRequestClose={props.handleClearSelectedOption}
-    contentLabel="Selected Option"
-    closeTimeoutMS={200}
-    className="modal"
-  >
-    {/* <h3 className="modal__title">Selected Option</h3> */}
-    {props.selectedOption && (
-      <p className="modal__body">{props.selectedOption}</p>
-    )}
-    <button className="button" onClick={props.handleClearSelectedOption}>
-      Okay
-    </button>
-  </Modal>
+  <SweetAlert
+    show={!!props.selectedOption}
+    title=""
+    text={props.selectedOption}
+    onConfirm={props.handleClearSelectedOption}
+  />
 );
-
-Modal.setAppElement("#root");
 
 export default OptionModal;
